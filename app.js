dc979e74d3982ff718f95cf2ba53030e86a77c59
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-03-05 03:35:03 +0530

    finishing touches to home page

--- app.js
+++ app.js
@@ -54,16 +54,26 @@ scrollLinks.forEach((link) => {
   });
 });
 
-// ********** typewriter ************
+// ********** set age ************
+const age = document.getElementById('age');
+// magic number: 31557600000 is 24 * 3600 * 365.25 * 1000 Which is the length of a year, the length of a year is 365 days and 6 hours which is 0.25 day. In the end i floor the result which gives us the final age.
+age.textContent = Math.floor(
+  (new Date() - new Date('2001-03-05').getTime()) / 3.15576e10
+);
 
-// let i = 0;
-// const myName = 'Durgesh';
-// window.addEventListener('DOMContentLoaded', typeWriter);
+// ********** read more ************
+const dots = document.getElementById('dots');
+const moreText = document.getElementById('more-text');
+const toggleTextBtn = document.getElementById('toggle-text');
 
-// function typeWriter() {
-//   if (i < myName.length) {
-//     document.getElementById('my-name').innerHTML += myName.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, 500);
-//   }
-// }
+toggleTextBtn.addEventListener('click', (e) => {
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline';
+    moreText.style.display = 'none';
+    toggleTextBtn.innerHTML = 'read more <i class="fas fa-angle-down"></i>';
+  } else {
+    moreText.style.display = 'inline';
+    dots.style.display = 'none';
+    toggleTextBtn.innerHTML = 'show less <i class="fas fa-angle-up"></i>';
+  }
+});
